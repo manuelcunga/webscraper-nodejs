4590b88b56dac7fc27168d373c00f1f3f0bb33dd
manuelcunga <manuelvianacunga@gmail.com>
2023-01-16 12:20:13 +0100

    feat: :art: First commit

---
+++ src/app.ts
@@ -0,0 +1,30 @@
+import './util/module-alias';
+import express from 'express';
+import { Server } from 'http';
+import router from './routes';
+
+export class SetupApplication {
+  private server?: Server;
+
+  constructor(private port = 3000, public app = express()) { }
+
+  public init(): void {
+    this.setupExpress();
+    this.setupRoutes();
+  }
+
+  private setupRoutes(): void {
+    this.app.use(router);
+  }
+
+  private setupExpress(): void {
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+  }
+
+  public start(): void {
+    this.server = this.app.listen(this.port, () => {
+      console.log(`Server running on port ${this.port}`);
+    });
+  }
+}
---
+++ src/router/router.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import ProductRouter from '@modules/Product/Router';
+
+class Routes {
+    static define(router: Router): Router {
+        router.use('/products', ProductRouter);
+
+        return router;
+    }
+}
+
+export default Routes.define(Router());
